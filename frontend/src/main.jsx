f723e74743e0ae9603c5fe729bf3862de36d68e2
Vo Duc Nam <nvo029566@gmail.com>
2026-09-21 12:59:00 +0700

    fix CORS, add favicon, fix vite config allowedHosts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import axios from 'axios';
 
-axios.defaults.headers.common["ngrok-skip-browser-warning"] = "true";
+axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
